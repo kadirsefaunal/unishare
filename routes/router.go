a3ea853c9f81e5d3d7954ca3f1edf67ea79f781f
kadirsefaunal <kadirsefau@gmail.com>
2019-01-04 10:48:54 +0300

    Class update endpoint added.

--- routes/router.go
+++ routes/router.go
@@ -22,4 +22,5 @@ func InitRoutes(e *echo.Echo) {
 	class := e.Group("/class", middlewares.CheckToken)
 	class.POST("", handlers.ClassCreate).Name = "create-class"
 	class.DELETE("/:id", handlers.ClassDelete).Name = "delete-class"
+	class.PUT("/:id", handlers.ClassUpdate).Name = "update-class"
 }
