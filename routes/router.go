951e1e89b81c720805085ca8cb9531af8591416e
kadirsefaunal <kadirsefau@gmail.com>
2019-01-07 13:39:59 +0300

    Post update endpoint added.

--- routes/router.go
+++ routes/router.go
@@ -29,4 +29,5 @@ func InitRoutes(e *echo.Echo) {
 	post := e.Group("/post", middlewares.CheckToken)
 	post.POST("", handlers.PostCreate).Name = "create-post"
 	post.GET("/:id", handlers.PostGet).Name = "get-post"
+	post.PUT("/:id", handlers.PostUpdate).Name = "update-post"
 }
