3d208281fb0741a03e56c0cd7025edf0f6e251c1
kadirsefaunal <kadirsefau@gmail.com>
2019-01-07 11:50:52 +0300

    Get post endpoint added.

--- repositories/post_repository.go
+++ repositories/post_repository.go
@@ -12,3 +12,13 @@ func PostInsert(post *models.Post) error {
 	err := db.Create(&post).Error
 	return err
 }
+
+func PostGet(postID string) (*models.Post, error) {
+	db := db.Connect()
+	defer db.Close()
+
+	post := new(models.Post)
+	err := db.Find(&post, "id = ?", postID).Error
+
+	return post, err
+}
